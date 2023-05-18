9a8c44fb0c1098b6a803c73c814eac2f6a287c25
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-18 21:16:11 +0530

    Added: Leap year program

--- src/com/bridglabz/BasicCoreJavaPrograms/BasicCoreJavaProgram.java
+++ src/com/bridglabz/BasicCoreJavaPrograms/BasicCoreJavaProgram.java
@@ -2,6 +2,7 @@ package com.bridglabz.BasicCoreJavaPrograms;
 public class BasicCoreJavaProgram {
     public static void main(String[] args) {
         flipCoin();
+        leapYear();
     }
     public static void flipCoin(){
         double flip=Math.random();
@@ -9,6 +10,14 @@ public class BasicCoreJavaProgram {
         System.out.println(flip+ " : tails");
         }else{
             System.out.println(flip+ " : head");
+        }
+    }
+    public static void leapYear(){
+        int year=2020;
+        if(((year%4==0)&&(year%100 !=0))||(year%400==0)){
+            System.out.println(year+ ": is Leap year");
+        }else{
+            System.out.println(year+ " : is not a leap year");
         }
     }
 }
