3204869f73e0ca1f02952d2c8b9c36063a0677cf
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-18 23:27:00 +0530

    Added: Even odd program

--- src/com/bridglabz/BasicCoreJavaPrograms/BasicCoreJavaProgram.java
+++ src/com/bridglabz/BasicCoreJavaPrograms/BasicCoreJavaProgram.java
@@ -7,7 +7,8 @@ public class BasicCoreJavaProgram {
         flipCoin();
         leapYear();
         powerOfTwo();
-        harmonicSum();
+//        harmonicSum();
+        evenOdd();
     }
     public static void flipCoin(){
         double flip=Math.random();
@@ -40,5 +41,13 @@ public class BasicCoreJavaProgram {
              System.out.println(sum);
              System.out.println(sum);
          }
+    }
+    public static void evenOdd(){
+        int num=3;
+        if(num%2==0){
+            System.out.println(num+ " is even number");
+        }else{
+            System.out.println(num+ " is odd number.");
+        }
     }
 }
